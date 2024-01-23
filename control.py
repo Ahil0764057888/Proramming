a2236dba36a239b4558ccc3de80c291b7122b43f
https://github.com/Ahil0764057888/programming.git <mohammedahil948@gmail.com>
2024-01-23 00:51:06 -0800

    add

---
+++ control.py
@@ -0,0 +1,9 @@
+a = int(input("Enter any number"))
+if (a > 60):
+    print("I like Iced creame")
+elif (a > 30):
+     print("I like choclate")
+elif  (a >20 ):
+    print ("I like apple")
+else:
+    print ("i do not anything")
